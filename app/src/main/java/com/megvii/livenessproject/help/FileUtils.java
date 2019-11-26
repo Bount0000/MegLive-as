51c8d7e5fb8ea311ee97f98852226ae103e2f1b6
Bount <15011462829@163.com>
2019-11-26 10:06:46 +0800

    Initial commit

---
+++ app/src/main/java/com/megvii/livenessproject/help/FileUtils.java
@@ -0,0 +1,130 @@
+package com.megvii.livenessproject.help;
+
+import android.content.Context;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+/**
+ * Created by zhangwenjun on 2018/1/17.
+ */
+
+public class FileUtils {
+    /**
+     * 根据byte数组，生成文件
+     */
+    public static String saveFile(Context mContext, byte[] data, String key,String suffix) {
+        if (data == null)
+            return null;
+
+        File mediaStorageDir = mContext.getExternalFilesDir("idcard");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        try {
+            String jpgFileName = "_" + key + suffix;
+            fos = new FileOutputStream(mediaStorageDir + "/" + jpgFileName);
+            bos = new BufferedOutputStream(fos);
+            bos.write(data);
+            return mediaStorageDir.getAbsolutePath() + "/" + jpgFileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return bytesToHexString(digest.digest());
+    }
+
+    public static String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+        for (int i = 0; i < src.length; i++) {
+            int v = src[i] & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 文件转化为字节数组
+     *
+     * @param file
+     * @return
+     */
+    public static byte[] getBytesFromFile(File file) {
+        byte[] ret = null;
+        try {
+            if (file == null) {
+                // log.error("helper:the file is null!");
+                return null;
+            }
+            FileInputStream in = new FileInputStream(file);
+            ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
+            byte[] b = new byte[4096];
+            int n;
+            while ((n = in.read(b)) != -1) {
+                out.write(b, 0, n);
+            }
+            in.close();
+            out.close();
+            ret = out.toByteArray();
+        } catch (IOException e) {
+            // log.error("helper:get bytes from file process error!");
+            e.printStackTrace();
+        }
+        return ret;
+    }
+}
